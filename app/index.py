45c91783b8f7a0d824a5baa2819826bfaa2bed7f
Jan Zaplatil <jan.zaplatil@gmail.com>
2020-04-21 14:53:32 +0200

    -a

--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, Markup
 import csv
 
 
@@ -11,29 +11,37 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/video')
+def video():
+    return render_template('video.html')
 
-@app.route('/blog')
+
+@app.route("/blog")
 def blog():
-    with open('static/clanek_noge_noge.csv') as csv_file:
-        data = csv.reader(csv_file, delimiter=',')
-        first_line = True
-        paragraphs = []
-        for row in data:
-            if not first_line:
-                paragraphs.append({
-                    "para":row[0]
-                })
-            else:
-                first_line = False
-        return render_template("blog.html", paragraphs=paragraphs)
+  with open('static/vaja_noge_popravljena.csv') as csv_file:
+    data = csv.reader(csv_file, delimiter=',') 
+    first_line = True
+    elements = []
+    for row in data:
+      if not first_line:
+        elements.append({
+          "hedaer": row[0],
+          "img_source": row[1],
+          "post": row[2]
+        })
+      else:
+        first_line = False
+  return render_template("blog.html", elements=elements)
+
 
 @app.route('/workout')
 def workout():
     return render_template('workout.html')
 
-@app.route('/video')
-def video():
-    return render_template('video.html')
+@app.route('/trainers')
+def trainers():
+    return render_template('trainers.html')
+
 
 
 
